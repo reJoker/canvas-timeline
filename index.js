f637681a5a764299553e76933a130e62bdf59025
reJoker <djack0812@gmail.com>
2016-01-11 01:21:03 +0800

    Setters and getters for domain and range.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 
-var canvasTimeline = function () {
+var canvasTimeline = function (options) {
+    var exports = {},
+        domain,
+        range
+        _rangeToDomain,
+        _domainToRange;
+
+    exports.domain = function () {
+        if (!arguments.length) {
+            return domain;
+        }
+        domain = arguments[0];
+        return this;
+    };
+
+    exports.range = function () {
+        if (!arguments.length) {
+            return range;
+        }
+        range = arguments[0];
+        return this;
+    }
+
+
+    return exports 
 }
 
 module.exports = canvasTimeline;
