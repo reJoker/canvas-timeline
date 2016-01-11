37efeb0bdc54edd62be1660441779b5b763d6b31
reJoker <djack0812@gmail.com>
2016-01-12 00:37:25 +0800

    Added transform functions.

--- index.js
+++ index.js
@@ -2,24 +2,47 @@
 var canvasTimeline = function (options) {
     var exports = {},
         domain,
-        range
+        range,
         _rangeToDomain,
-        _domainToRange;
+        _domainToRange,
+        _transform;
+
+    _transform = function (arr) {
+        var strart = arr[0],
+            end = arr[1],
+            distance = end - start;
+        return {
+            start: start,
+            end: end,
+            distance: distance; 
+            ratio: function (x) {
+                return (x - start) / distance;
+            }
+        };
+    };
+
+    _rangeToDomain = function (r) {
+        return range.ratio(r) * domain.distance + domain.start;
+    };
+
+    _domainToRange = function (d) {
+        return domian.ratio(d) * range.distance + range.start;
+    }
 
     exports.domain = function () {
         if (!arguments.length) {
             return domain;
         }
-        domain = arguments[0];
-        return this;
+        domain = _transform(arguments[0]);
+        return exports;
     };
 
     exports.range = function () {
         if (!arguments.length) {
             return range;
         }
-        range = arguments[0];
-        return this;
+        range = _transform(arguments[0]);
+        return exports;
     }
 
 
